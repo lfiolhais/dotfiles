e2e8be9cca15b795b1f671aebf67451e0d39b3e5
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2024-10-02 22:01:34 +0100

    Update tmux config
    Add fix for tmux-resurrect plugin

--- run_once_after_install-05-setup-tpm.sh
+++ run_once_after_install-05-setup-tpm.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+~/.tmux/plugins/tpm/bin/install_plugins
+~/.tmux/plugins/tpm/bin/update_plugins all
+
+# Apply Git patch to fix tmux-resurrect
+cd ~/.tmux/plugins/tmux-resurrect/ && git apply ~/.config/tmux-resurrect-patch.diff && cd -
