7dffdd5b981424cd7480965d87089130fc50719b
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2024-11-13 20:28:40 +0000

    Source tmux config before executing TPM scripts

--- run_once_after_install-05-setup-tpm.sh
+++ run_once_after_install-05-setup-tpm.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Source config
+tmux -c 'tmux source .tmux.conf'
+
 ~/.tmux/plugins/tpm/bin/install_plugins
 ~/.tmux/plugins/tpm/bin/update_plugins all
 
