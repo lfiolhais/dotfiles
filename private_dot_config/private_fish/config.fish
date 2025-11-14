a852f4772d6cd6f70a9513b27f85516cf74099a3
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2025-11-14 13:07:52 +0000

    Be brave and stop using tmux

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,22 +6,5 @@ source $HOME/.config/fish/aliases.fish
 source $HOME/.config/fish/greet.fish
 source $HOME/.config/fish/colors.fish
 
-# Spawn tmux
-if status --is-interactive
-    tmux 2> /dev/null; and exec true
-end
-
-# Ensure ssh agent is running
-if test -z "$SSH_ENV"
-    set -xg SSH_ENV $HOME/.ssh/environment
-end
-
-if not __ssh_agent_is_started
-    __ssh_agent_start
-end
-
-# Ensure tmux continuum is running
-tmux-continuum
-
 starship init fish | source
 
