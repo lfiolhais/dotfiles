adb58f9c869978fed90a6649642347c8db541406
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2025-11-22 18:22:30 +0000

    Fish: Attempt to remove sponsors in `yt-dlp`

--- private_dot_config/private_fish/functions/yt-dlp.fish
+++ private_dot_config/private_fish/functions/yt-dlp.fish
@@ -1,5 +1,4 @@
 function yt-dlp
-    # /usr/bin/yt-dlp -N6 -f 'bestvideo*+bestaudio/best' --merge-output-format mp4 --recode-video mp4 --postprocessor-args "VideoConvertor: -c:v h264_nvenc -preset:v p7 -tune:v hq -rc:v vbr -cq:v 32 -b:v 0 -profile:v" $argv
-    /opt/homebrew/bin/yt-dlp -N6 -f 'bestvideo*+bestaudio/best' --merge-output-format mkv $argv
+    /opt/homebrew/bin/yt-dlp -N6 --sponsorblock-mark all,-preview,-sponsor -t mkv $argv
 end
 
