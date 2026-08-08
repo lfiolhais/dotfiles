65087808935c3e2d0e3c3a90cde8946e939b3b74
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2026-08-08 08:46:17 +0100

    feat: add linux profiles with and without sudo

--- run_once_after_install-04-setup-fish.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-which fish | sudo tee -a /etc/shells
-
-chsh -s "$(which fish)"
--- run_once_after_install-05-setup-bat.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-bat cache --build
