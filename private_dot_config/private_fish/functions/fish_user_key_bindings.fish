4571a71839a86eeb1f8551ef367035b02a605a9c
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2026-09-18 18:59:11 +0100

    improve installation robustness

--- private_dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,5 @@
 function fish_user_key_bindings --description "Key bindings fish loads at startup: fzf's Ctrl-T/Ctrl-R/Alt-C"
-  fzf_key_bindings
+  # config.fish defines fzf_key_bindings only where fzf is installed and new
+  # enough for `fzf --fish`; without it there are no bindings to add.
+  functions --query fzf_key_bindings; and fzf_key_bindings
 end
